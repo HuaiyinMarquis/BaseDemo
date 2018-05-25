d363d593689d31792819266234459eb0db3a0d91
HuaiyinMarquis <HuaiyinMarquis@qq.com>
2018-05-25 17:34:51 +0800

    temp ... （unimportant）

--- src/main/java/com/springTest/controller/SimpleController.java
+++ src/main/java/com/springTest/controller/SimpleController.java
@@ -3,21 +3,34 @@ package com.springTest.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class SimpleController {
     Logger logger = LoggerFactory.getLogger(SimpleController.class);
 
-    @RequestMapping("/")
+    @RequestMapping("/testLog")
     @ResponseBody
-    String home() {
+    String testLog() {
         logger.trace("---------这是一个级别{}的日志-----------","trace");
         logger.debug("---------这是一个级别{}的日志-----------","debug");
         logger.info("---------这是一个级别{}的日志-----------","info"); //spring Boot 默认是info级别
         logger.warn("---------这是一个级别{}的日志-----------","warn");
         logger.error("---------这是一个级别{}的日志-----------","error");
-        return "Hello World!";
+        return "success";
+    }
+
+    @RequestMapping("/thymeleafDemo")
+    public String thymeleafDemo(Model model) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("name","张三");
+        map.put("age",18);
+        model.addAllAttributes(map);
+        return "thymeleafDemo";
     }
 }
